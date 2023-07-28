37236387fe6a3e0ef2787bc1f09d3baacba8e5ab
Suzane Duarte <ssuzane9@hotmail.com>
2023-07-27 22:56:29 -0300

    adicionando página de disclaimer

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,6 +1,24 @@
-import { Container } from '@mui/system'
+import { Box, Typography } from '@mui/material'
 
 const AboutPage = () => {
-	return <Container>Sobre o projeto:</Container>
+	return (
+		<Box
+			display='flex'
+			flexDirection='column'
+			alignItems='center'
+			justifyContent='center'
+			height='100vh'
+		>
+			<Typography marginTop='100px' textAlign='center' fontSize='42px'>
+				Sobre o projeto
+			</Typography>
+			<Box
+				display='flex'
+				alignItems='center'
+				justifyContent='center'
+				flex={1}
+			></Box>
+		</Box>
+	)
 }
 export default AboutPage
---
+++ pages/disclaimer/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+
+interface DisclaimerProps {
+	open: boolean
+	onAccept: () => void
+}
+
+const Disclaimer = () => {
+	return <Box>('oi')</Box>
+}
+
+export default Disclaimer
